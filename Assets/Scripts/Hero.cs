43b19e8bc4801f147457ec9a667b0a2b8ae86242
TravisTX <travistx@gmail.com>
2014-09-20 18:22:31 -0400

    Hero

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hero : MonoBehaviour
+{
+    private float _speed = 5.0f;
+
+    void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (InputManager.Instance.Touching())
+        {
+            Vector2 touchPosition = InputManager.Instance.GetTouch();
+            Vector3 touchWorldPoint = Camera.main.ScreenToWorldPoint(new Vector3(touchPosition.x, touchPosition.y, 0));
+
+            var currentDestination = new Vector3(touchWorldPoint.x, touchWorldPoint.y, transform.position.z);
+            Debug.DrawLine(transform.position, currentDestination, Color.white);
+            transform.position = Vector3.Lerp(transform.position, currentDestination, _speed * Time.deltaTime);
+            //transform.forward = currentDestination - transform.position;
+            //transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager
+{
+    public static InputManager Instance
+    {
+        get
+        {
+            return _instance ?? (_instance = new InputManager());
+        }
+    }
+    private static InputManager _instance;
+
+    public bool Touching()
+    {
+        if (Input.GetKey(KeyCode.Space))
+            return true;
+
+        if (Input.GetMouseButton(0))
+        {
+            return true;
+        }
+
+        if (Input.touchCount > 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public Vector2 GetTouch()
+    {
+        if (Input.touchCount > 0)
+        {
+            return Input.GetTouch(0).position;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            return Input.mousePosition;
+        }
+
+        return new Vector2();
+    }
+
+
+}
